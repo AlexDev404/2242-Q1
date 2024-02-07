f4a8240dc4e24acaf6c1131657927fae0556634e
AlexDev404 <alexdev404@noreply.users.github.com>
2024-02-07 13:07:33 -0600

    #8 - New method 'perimeter'

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
+
 import "math"
 import "fmt"
 
 // 1. Create a struct called 'triangle' that has two data fields: 'base' and 'height' both of type float64
 type triangle struct {
-	base float64
+	base   float64
 	height float64
 }
 
@@ -15,17 +16,24 @@ func (t triangle) area() float64 {
 
 // 3. Create a method on type 'triangle' named 'perimeter' that calculates and returns the perimeter of the triangle
 func (t triangle) perimeter() float64 {
-	return 2 * t.base + 2 * t.height
+	return 2*t.base + 2*t.height
 }
 
 // 6. Create a new type called 'circle' that has a single field of type float64 of name 'radius'
 type circle struct {
 	radius float64
 }
+
 // 7. Create a method on type 'circle' with the name 'area' that calculates and returns the area of a circle
 func (c circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
+
+// 8. Create a method on type 'circle' with the name 'perimeter' that calculates and returns the perimeter of a circle
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
 
 // 4. Inside the 'main' function create a variable of type 'triangle' and initialize it with the values for the two data fields
 func main() {
@@ -33,7 +41,5 @@ func main() {
 	// 5. Call the 'area' and 'perimeter' methods and print the results
 	fmt.Println(t.area())
 	fmt.Println(t.perimeter())
-
-	
 
 }
