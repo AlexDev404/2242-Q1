791e5909b219d830dcd428868f820f82146acad3
AlexDev404 <alexdev404@noreply.users.github.com>
2024-02-07 13:00:02 -0600

    Triangle!

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
+import "math"
+import "fmt"
+
+// 1. Create a struct called 'triangle' that has two data fields: 'base' and 'height' both of type float64
+type triangle struct {
+	base float64
+	height float64
+}
 
 func main() {
-
 }
